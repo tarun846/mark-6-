ca5b4a7f9a01799a147aaec77be048fcdc9dd9e1
tarun846 <jjagannathbehera896@gmail.com>
2022-09-05 06:21:10 +0530

    first

--- Script/script.js
+++ Script/script.js
@@ -4,13 +4,12 @@ const languageinput = document.querySelector('.language');
  
 async function languageconvertor(val) {
      try {
-        const data = await fetch(`https://api.funtranslations.com/translate/minion.json?text=${val}`)
+        const data = await fetch(`https://api.funtranslations.com/translate/pirate.json?text=${val}`)
         const datavalue  = await data.json()
         languageinput.textContent =  datavalue.contents.translated
     } catch (error) {
         alert("some error occured")
         console.error(error);
-        
      }
  }
 
